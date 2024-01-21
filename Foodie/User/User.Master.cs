a549cb6f9f8b3d81a396094a967eca9e5ef88880
kssharvanthika <kssharvanthika123@gmail.com>
2024-01-21 17:02:01 +0530

    Add project files.

---
+++ Foodie/User/User.Master.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Foodie.User
+{
+    public partial class User : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!Request.Url.AbsoluteUri.ToString().Contains("Default.aspx"))
+            {
+                form1.Attributes.Add("class", "sub_page");
+            }
+            else
+
+            {
+                form1.Attributes.Remove("class");
+                //load the control
+                Control sliderUserControl = (Control)Page.LoadControl("SliderUserControl1.ascx");
+                //add the control to the panel
+                pnSliderUC.Controls.Add(sliderUserControl);
+            }
+            if (Session["userId"]!=null)
+            {
+                lbloginOrLogout.Text = "Logout";
+               Utils utils = new Utils();
+                Session["cartCount"] = utils.cartCount(Convert.ToInt32(Session["userId"])).ToString(); 
+            }
+            else
+            {
+                lbloginOrLogout.Text = "Login";
+                Session["cartCount"] = "0";
+            }
+        
+       }
+
+        protected void lbloginOrLogout_Click(object sender, EventArgs e)
+        {
+            if (Session["userId"]== null)
+            {
+                Response.Redirect("Login.aspx");
+            }
+            else
+            {
+                Session.Abandon();
+                Response.Redirect("Login.aspx");
+            }
+        }
+
+        protected void lblRegisterOrProfie_Click(object sender, EventArgs e)
+        {
+            if (Session["userId"] != null)
+            {
+                lblRegisterOrProfie.ToolTip = "User Profile";
+                Response.Redirect("Profile.aspx");
+            }
+            else
+            {
+                lblRegisterOrProfie.ToolTip = "User Registration";
+                Response.Redirect("Registration.aspx");
+            }
+        }
+    }
+}
---
+++ Foodie/WebForm1.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Foodie.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <script type="text/javascript">
+        function Showalert() {
+            alert('Call JavaScript function from codebehind');
+        }
+    </script>
+</head>
+
+<body>
+    <form id="form1" runat="server">
+        <div>
+                    
+            <asp:TextBox runat="server"></asp:TextBox>
+            
+                            <asp:Repeater  runat="server" DataSourceID="SqlDataSource1">
+                             </asp:Repeater>
+                                        
+            <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:FoodieDBConnectionString %>" SelectCommand="SELECT * FROM [Carts]"></asp:SqlDataSource>
+                                        
+            <asp:Button ID="Button1" runat="server" Text="Showalert" OnClick="Button1_Click" />
+                                        
+        </div>
+    </form>
+</body>
+</html>
